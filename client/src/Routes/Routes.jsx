ba5395f336df8eab55e77b65e4893c5d2369d5a3
Keshav Kumar <104895178+BitcodeVelocity@users.noreply.github.com>
2024-01-01 17:09:38 +0530

    Feat!:By deafult root route navigation!

--- client/src/Routes/Routes.jsx
+++ client/src/Routes/Routes.jsx
@@ -1,12 +1,11 @@
 import React, { createContext, useEffect, useState } from 'react'
-import { Routes, Route, Link, useNavigate } from 'react-router-dom'
+import { Routes, Route,useNavigate,Navigate } from 'react-router-dom'
 import Login from '../pages/LoginPage/Login'
 import Register from '../pages/RegisterPage/Register'
 import AddJob from '../pages/AddJobPage/AddJob'
 import JobDescription from '../pages/JobDescriptionPage/JobDescription'
 import Home from '../pages/HomePage/Home'
 export const AuthContext = createContext()
-
 const Router = () => {
   const [isLogin, setIsLogin] = useState()
   const [recruiterName, setRecruiterName] = useState('')
@@ -17,22 +16,21 @@ const Router = () => {
       if (token) {
         setIsLogin(true);
         setRecruiterName(token.recruiterName);
-      }else {
+      } else {
         setIsLogin(false)
       }
-      navigate('/all-jobs');
     };
     checkAuthentication();
   }, [isLogin])
 
   return (
     <>
-      <AuthContext.Provider value={{ isLogin: isLogin,setIsLogin: setIsLogin, recruiterName: recruiterName }}>
+      <AuthContext.Provider value={{ isLogin: isLogin, setIsLogin: setIsLogin, recruiterName: recruiterName }}>
         <Routes>
-          <Route path='/' element={<h1>Welcome To HireZoom! <Link to='/register'>Click me</Link></h1>}></Route>
+          <Route path='/' element={<Navigate to="/all-jobs" />}></Route>
           <Route path='/login' element={<Login />}></Route>
           <Route path='/register' element={<Register />}></Route>
-          <Route path='/create-job' element={<AddJob />}></Route>
+          <Route path='/create-job/:id?' element={<AddJob />}></Route>
           <Route path='/job-details/:id' element={<JobDescription />}></Route>
           <Route path='/all-jobs' element={<Home />}></Route>
         </Routes>
